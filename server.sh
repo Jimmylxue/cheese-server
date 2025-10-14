904c87ea4a800d59115a63b6dde97b2a11b78461
jimmyxuexue <1002661758@qq.com>
2025-10-14 09:46:50 +0800

    feat: 打包模块更新

---
+++ server.sh
@@ -0,0 +1,20 @@
+# 切换分支
+git checkout server-prod
+
+# 清空当前工作区
+git checkout .
+
+# 拉取代码
+git pull --rebase origin server-prod
+
+# 安装依赖
+pnpm install
+
+# 构建
+pnpm build
+
+# 删除pm2 容器
+pm2 delete chess-api-server
+
+# pm2 运行
+pm2 start dist/main.js --name "chess-api-server"
